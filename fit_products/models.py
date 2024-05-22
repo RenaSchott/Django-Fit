b577154e9f158b1385ab464182d61bd2decbadff
R S <rena_schott@gmx.de>
2024-05-22 14:56:30 +0000

    Added MainCategory model to new fit_products app

---
+++ fit_products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class MainCategory(models.Model):
+    """ 
+    Stores a single category
+    """
+    class Meta:
+        verbose_name_plural = 'MainCategories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
