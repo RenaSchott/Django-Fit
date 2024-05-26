53f6f28cab008f750884dbe1252fa1292736ac42
R S <rena_schott@gmx.de>
2024-05-26 07:44:36 +0000

    Added first attempt for product model to fit_product models

--- fit_products/models.py
+++ fit_products/models.py
@@ -26,7 +26,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    main_category = models.ManyToManyField(MainCategory)
+    main_category = models.OneToManyField(MainCategory)
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254)
 
@@ -35,3 +35,20 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+class Product(models.Model):
+    """
+    Stores a single product
+    """
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    sku = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    trainer = models.TextField(max_digits=6, decimal_places=2, null=True)
+    description = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
