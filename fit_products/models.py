061bf1be55f9a823b4c15b9649526405ba0c89cc
R S <rena_schott@gmx.de>
2024-05-26 07:28:55 +0000

    Added category model to fit_product models

--- fit_products/models.py
+++ fit_products/models.py
@@ -4,11 +4,29 @@ from django.contrib.auth.models import User
 
 class MainCategory(models.Model):
     """ 
-    Stores a single category
+    Stores a single main category
     """
     class Meta:
         verbose_name_plural = 'MainCategories'
 
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Category(models.Model):
+    """ 
+    Stores a single category
+    """
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    main_category = models.ManyToManyField(MainCategory)
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254)
 
